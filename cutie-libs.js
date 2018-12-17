aedcf0938679d18fae596a1e9968bffbdcf34db1
gismayilov <guseynism@gmail.com>
2018-12-17 14:57:19 +0300

    page logo

---
+++ cutie-libs.js
@@ -0,0 +1,35 @@
+// returns a window with a document and an svg root node
+const window  = require('svgdom');
+const SVG  = require('svg.js')(window);
+const document = window.document;
+const fs = require('fs');
+
+// create svg.js instance
+const draw = SVG(document.documentElement);
+
+// use svg.js as normal
+draw.line(10, 11, 80, 11).stroke({ width: 2, color: '#336699' });
+draw.line(11, 11, 11, 40).stroke({ width: 2, color: '#336699' });
+draw.line(11, 39, 80, 39).stroke({ width: 2, color: '#336699' });
+draw.rect(50, 30).move(80, 10).fill('#336699');
+var pageText = draw.text(function(add){
+  add.tspan('cutie').fill('#333')
+}).move(46, 15);
+pageText.font({
+  family: 'Helvetica',
+  size: 24,
+  anchor: 'middle',
+  leading: '1.5em'
+});
+
+var libsText = draw.text(function(add){
+  add.tspan('libs').fill('#fff')
+}).move(106, 15);
+libsText.font({
+  family: 'Helvetica',
+  size: 24,
+  anchor: 'middle',
+  leading: '1.5em'
+});
+
+fs.writeFileSync('./cutie-libs.html', draw.svg());
--- cutie.js
+++ cutie.js
@@ -5,31 +5,22 @@ const document = window.document;
 const fs = require('fs');
 
 // create svg.js instance
-const draw = SVG(document.documentElement);
+const draw = SVG(document.documentElement).size(230, 180);
 
-// use svg.js as normal
-draw.line(10, 11, 80, 11).stroke({ width: 2, color: '#336699' });
-draw.line(11, 11, 11, 40).stroke({ width: 2, color: '#336699' });
-draw.line(11, 39, 80, 39).stroke({ width: 2, color: '#336699' });
-draw.rect(50, 30).move(80, 10).fill('#336699');
-var pageText = draw.text(function(add){
-  add.tspan('cutie').fill('#333')
-}).move(46, 15);
-pageText.font({
-  family: 'Helvetica',
-  size: 24,
-  anchor: 'middle',
-  leading: '1.5em'
+const path = draw.path('M 10 30 C 10 30 120 120 170 90 L -50 200')
+path.fill('none').move(10, 10)
+path.stroke({ color: '#333', width: 3, linecap: 'round', linejoin: 'round' })
+
+const text = draw.text(function(add) {
+  add.tspan('Page').fill('#C40233');
 });
-
-var libsText = draw.text(function(add){
-  add.tspan('libs').fill('#fff')
-}).move(106, 15);
-libsText.font({
-  family: 'Helvetica',
-  size: 24,
-  anchor: 'middle',
-  leading: '1.5em'
+text.move(30, 132).rotate(-26);
+text.font({
+  family:   'Lucida Grande',
+  color: 'red',
+  size:     28,
+  anchor:   'middle',
+  leading:  '1.5em'
 });
 
 fs.writeFileSync('./cutie.html', draw.svg());
