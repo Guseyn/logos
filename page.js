93dec2d8c0077623b8b9af1b405aa600f62828e1
gismayilov <guseynism@gmail.com>
2018-12-27 13:39:51 +0300

    imroved page logo

--- page.js
+++ page.js
@@ -9,17 +9,17 @@ const draw = SVG(document.documentElement).size(230, 180);
 
 const path = draw.path('M 10 30 C 10 30 120 120 170 90 L -50 200')
 path.fill('none').move(10, 10)
-path.stroke({ color: '#333', width: 3, linecap: 'round', linejoin: 'round' })
+path.stroke({ color: '#E0E0E0', width: 1, linecap: 'round', linejoin: 'round' })
 
 const text = draw.text(function(add) {
   add.tspan('Page').fill('#C40233');
 });
-text.move(30, 132).rotate(-26);
+text.move(38, 130).rotate(-26);
 text.font({
-  family:   'Lucida Grande',
-  size:     28,
-  anchor:   'middle',
-  leading:  '1.5em'
+  family: 'Lucida Grande',
+  size: 26,
+  anchor: 'middle',
+  leading: '1.5em'
 });
 
 fs.writeFileSync('./page.svg', draw.svg());
