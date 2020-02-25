ae6afffd19a35143c1b8eae2ad75d343f0d375c0
guseyn <guseynism@gmail.com>
2020-02-25 10:59:53 +0300

    fix circle backgrounds

--- cutie.js
+++ cutie.js
@@ -10,14 +10,14 @@ const draw = SVG(document.documentElement).size(180, 160);
 const blue = '#82BCE5';
 const green = '#CAE595';
 const grey = '#999';
-const white = '#fdfdfd';
+const white = 'none';
 draw.circle(30).move(60, 10).fill(blue);
-draw.circle(30).move(30, 65).fill(blue).stroke({ width: 1, color: blue });
+draw.circle(28).move(30, 65).fill('none').stroke({ width: 4, color: blue });
 draw.circle(20).move(35, 70).fill(white);
 draw.circle(30).move(90, 65).fill(blue);
-draw.circle(30).move(60, 120).fill(blue).stroke({ width: 1, color: blue });
+draw.circle(28).move(60, 120).fill('none').stroke({ width: 4, color: blue });
 draw.circle(20).move(65, 125).fill(white);
-draw.circle(30).move(120, 120).fill(blue).stroke({ width: 1, color: blue });
+draw.circle(28).move(120, 120).fill('none').stroke({ width: 4, color: blue });
 draw.circle(20).move(125, 125).fill(white);
 
 fs.writeFileSync('./cutie.svg', draw.svg());
